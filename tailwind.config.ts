ca0b0ca843fb7f079f02fd9ec61a9cd7d6002f11
Nathan Osman <nathan@nathanosman.com>
2026-05-16 15:51:25 -0700

    Add Tailwind configuration.

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        background: "#232323",
+        "foreground-primary": "#ffffff",
+        "foreground-secondary": "#9d9d9d",
+        primary: "#0084ff",
+        secondary: "#69b3ff",
+        panel: "#313131",
+      },
+    },
+  },
+}
