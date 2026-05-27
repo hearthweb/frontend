4d007bcee1c7a01b5897f6371b4ac5ef54529ee1
Nathan Osman <nathan@nathanosman.com>
2026-05-26 22:36:22 -0700

    Add nuxt-auth.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,30 @@ export default defineNuxtConfig({
     viteEnvironmentApi: true,
   },
 
-  modules: ["@nuxt/icon", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/icon", "@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
+
+  auth: {
+    // Protect all pages by default
+    globalAppMiddleware: true,
+
+    provider: {
+      // Use the local provider
+      type: "local",
+
+      // Specify the prefix for the routes
+      baseURL: "/api/auth",
+
+      // API endpoints for login, logout, and session
+      endpoints: {
+        signIn: { path: "/login", method: "post" },
+        signOut: { path: "/logout", method: "post" },
+        getSession: { path: "/me", method: "get" },
+      },
+
+      // Specify the login page to redirect to
+      pages: {
+        login: "/login",
+      },
+    },
+  },
 })
