83b9132bbb44a61881e2575c1f133938194dc4f9
Nathan Osman <nathan@nathanosman.com>
2026-05-27 01:41:27 -0700

    Add input and button components.

---
+++ app/components/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="rounded bg-primary px-8 py-2 text-sm font-bold text-primary-contrast hover:bg-secondary focus:outline focus:outline-2 focus:outline-foreground-primary"
+  >
+    <slot />
+  </button>
+</template>
