ce8fa32cc31cc14cedd6c6104d1aaf08776b6e05
Nathan Osman <nathan@nathanosman.com>
2026-05-27 01:52:57 -0700

    Add auth layout.

---
+++ app/layouts/auth.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center bg-panel px-4">
+    <div class="w-full max-w-md rounded bg-background p-8 shadow-xl">
+      <slot />
+    </div>
+  </div>
+</template>
