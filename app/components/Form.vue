96edc3768301fb726e438892d29a3984c5b2f4b8
Nathan Osman <nathan@nathanosman.com>
2026-05-27 01:50:55 -0700

    Add Form component.

---
+++ app/components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col gap-y-8">
+    <div>
+      <div class="text-xl">{{ title }}</div>
+      <div class="text-sm text-foreground-secondary">{{ description }}</div>
+    </div>
+    <Fields>
+      <slot />
+    </Fields>
+    <Buttons>
+      <Button>{{ action }}</Button>
+    </Buttons>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title: string
+    description: string
+    action?: string
+  }>(),
+  {
+    action: "Submit",
+  },
+)
+</script>
