915c611583d9bfdf6c0942ee6cf55842f4083b7d
Nathan Osman <nathan@nathanosman.com>
2026-05-27 01:53:49 -0700

    Add login page.

---
+++ app/pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <Form
+    title="Login to Hearth"
+    description="Please enter your email address and password"
+    action="Login"
+  >
+    <Input name="email" label="Email" placeholder="user@example.com" />
+    <Input
+      type="password"
+      name="password"
+      label="Password"
+      placeholder="*****"
+    />
+    <div class="text-sm text-foreground-secondary">
+      TODO: forgot your password?
+    </div>
+  </Form>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "auth",
+})
+</script>
